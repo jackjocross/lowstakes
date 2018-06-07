02d5cf6835b05550b1cee60cc04baa13d18ac14f
jack <jackjocross@gmail.com>
2018-06-06 23:45:42 -0400

    downgrade to gatsby v1

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-contentful`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,32 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { StaticQuery } from 'gatsby'
-// import Link from 'gatsby-link'
 
-export default ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query IndexQuery {
-        allContentfulArticle {
-          edges {
-            node {
-              id
-            }
-          }
-        }
-        site {
-          siteMetadata {
-            title
-          }
+const IndexPage = ({ data }) => (
+  <div>
+    <Helmet
+      titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+      defaultTitle={data.site.siteMetadata.title}
+    />
+
+    <div>{JSON.stringify(data, null, 2)}</div>
+  </div>
+)
+
+export default IndexPage
+
+export const query = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulArticle {
+      edges {
+        node {
+          id
         }
       }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-          defaultTitle={data.site.siteMetadata.title}
-        />
-
-        <div>{JSON.stringify(data, null, 2)}</div>
-      </>
-    )}
-  />
-)
+    }
+  }
+`
